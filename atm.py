b69a7b78d1464321cfcc0de5d3765758216fe928
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-10-03 11:15:31 +0530

    first commit

--- atm.py
+++ atm.py
@@ -13,18 +13,15 @@ __        __   _                            _____          _  _____ __  __
 
 card_inset = str(input("Insert a card : "))
 
-# chack card condition
 if card_inset == "yes":
     user_pin = int(input("Enter a 4 digit pin : "))
 
-    # chack PIN condition
     if user_pin == pin:
         print("Select a language")
         print("1.English")
         print("2.Kannada")
         language_inp = int(input("Enter 1 or 2: "))
 
-        # chack lan condition
         while True:
             if language_inp == 1:
                 print("Select English")
@@ -33,31 +30,19 @@ if card_inset == "yes":
                 print("1.Withdraw")
                 print("2.Deposit")
                 print("3.Balance Enquiry")
-
                 options_inp = int(input("Enter your option : "))
 
-                # deposit option condition
-                if options_inp == 2:
-                    dep_amo = int(input("Enter a amount to deposit :  "))
-                    bal = bal + dep_amo
-                    print("Your balance after a deposit : ", bal)
-                    break
-
-                # chack option condition
                 if options_inp == 1:
                     print("Enter type of account")
                     print("1.Saving account")
                     print("2.Current account")
                     account_typ = int(input("Enter your account type : "))
 
-                    # chack account condition
                     if account_typ == 1:
                         amount = int(input("Enter a amount : "))
                         print("Please wait")
                         print("Collect your cash")
                         balance = str(input("Do you want to see balance (Yes/No) : "))
-
-                        # chack balance condition
                         if balance == "yes":
                             print("Your balance amount : ", bal - amount)
                             con_user = str(
