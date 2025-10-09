c23a3dbea6e332dc2ed8df855aec3b54fd20ed64
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-10-09 20:16:32 +0530

    first commit

--- app.py
+++ app.py
@@ -175,3 +175,195 @@
 # print(id(a))
 # print(id(b))
 # print(a is b)
+
+
+#
+# l1 = [1,3,8,-9]
+# print(l1[1])
+# print(l1[3])
+# print(3 in l1)
+# print([-9] in l1)
+# a=(2*4)
+# print(a not in l1)
+# print(8 in a)
+#
+# print("8" in "88")
+
+# a = 5
+# b = (4+1)
+# c = (5-0)
+# print(a is 5)
+# print(a is not "b")
+# print("===================")
+# print(a is b)
+# print(b is not c)
+# print(a is id(5))
+#
+# class cal:
+#     def __init__(self):
+#         self.a = 100
+#         self.b = 200
+#
+#     def add(self):
+#         x = 10
+#         y = 20
+#         z = x + y
+#         return  z
+#
+# c1 = cal()
+# print(c1.a)
+# print(c1.b)
+# res1 = c1.add()
+# print(res1)
+#
+#
+# n1 = int(input("Enter num 1 : "))
+# n2 = int(input("Enter num 2 : "))
+# n3 = int(input("Enter num 3 : "))
+#
+# if n2 < n1 > n3:
+#     print(n1)
+# elif n1 < n2 > n3:
+#     print(n2)
+# else:
+#     print(n3)
+# print("Calcutied..........")
+
+#
+# n1 = int(input("Enter a number : "))
+# #
+# # if n1 <= 0:
+# #     print("Neutral")
+# # elif n1 % 3 == 0 and n1 % 5 == 0:
+# #     print("Fizz Buzz Num")
+# # elif n1 % 5 == 0:
+# #     print("Buzz Num")
+# # elif n1 % 3 == 0:
+# #     print("Fizz Num")
+# # else:
+# #     print("You have entered a number")
+#
+# if n1 > 0:
+#     if n1 % 3 == 0 and n1 % 5 == 0:
+#         print("Fizz Buzz")
+#     elif n1 % 3 == 0:
+#         print("Fizz")
+#     elif n1 % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print("You have entered a number")
+# else:
+#     print("Neutral")
+#
+# class cal:
+#     def __init__(self):
+#         self.a = 10
+#         self.b = 20
+#     def add(self,x,y):
+#         z = x+y
+#         return  z
+#
+# c1 = cal()
+# print(c1.a)
+# print(c1.b)
+# res1 = c1.add(111,222)
+# print(res1)
+#
+# class cla:
+#     def __init__(self):
+#         self.x = 10
+#         self.y = 20
+#     def add(self):
+#         self.x=10
+#         self.=30
+#         c=a+b
+#         print("the sum is",c)
+#     def diff(self):
+#         n1 = 50
+#         n2 = 40
+#         n3 = n1 - n2
+#         print("The diff is" , n3)
+#
+# c1 = cla()
+# c1.add()
+# c1.diff()
+#
+# class student:
+#     def __init__(self):
+#         self.name = "Rama"
+#         self.age = 22
+#         self.height = 5.6
+#
+#     def display(self):
+#         print(self.name )
+#         print(self.height )
+#         print(self.age )
+#
+# s1 = student()
+# s1.display()
+# print(s1.name)
+# print(s1.age)
+# print(s1.height)
+
+# var = range(7)
+# print(range(1,5))
+# print(range(10,15,2))
+# print(range(7))
+# print(range())
+# n = int(input("Enter a num :"))
+# for i in range(n,(1-1),-2):
+#     print(i,end=" ")
+# print("\nProg exec")
+# print("last updated value :",i)
+#
+# class Heroine:
+#     def __init__(self):
+#         self.name = "Ramya"
+#         self.age = 32
+#         self.mod = 1234
+#
+#     def act(self):
+#         self.heigh = 5.6
+#         self.color = "White"
+#
+# h1 = Heroine()
+# print(h1.name)
+# print(h1.age)
+# print(h1.mod)
+# h1.act()
+# h1.name = "Radhika"
+# h1.addr = "Bang"
+# print(h1.name)
+# print(h1.age)
+# print(h1.mod)
+# print(h1.addr)
+#
+# class Student:
+#     def __init__(self,name,age,height,addr):
+#         self.name = name
+#         self.age = age
+#         self.height = height
+#         self.addr = addr
+#
+# s1=Student("rama",23,5.8,"Beng")
+# s2=Student("sita",22,5.6,"chin")
+#
+# print(s1.name,s1.age)
+# print(s2.name,s2.age)
+
+class Demo:
+    x = 99
+    y = 88
+    def __init__(self):
+        self.a=10
+        self.b = 20
+
+d1=Demo()
+d2=Demo()
+d3=Demo()
+print(Demo.x)
+print(Demo.y)
+Demo.x = 889
+Demo.y = 888
+print(Demo.x)
+print(Demo.y)
