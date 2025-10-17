0a82b2ae358468ccf349981fa8d3a67d9c0ff2aa
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-10-17 22:16:16 +0530

    first commit

--- DSA/app.py
+++ DSA/app.py
@@ -1,8 +1,8 @@
 arr = [1, 2, 1, 2, 3, 3, 16, 17, 18, 78]
 
 arr_sorted = sorted(arr)
-for i in range(len(arr_sorted) - 2, -1, -1):  # loop backward
-    if arr_sorted[i] < arr_sorted[-1]:  # find next smaller than max
+for i in range(len(arr_sorted) - 2, -1, -1):
+    if arr_sorted[i] < arr_sorted[-1]:
         print(arr_sorted[i])
         break
     else:
