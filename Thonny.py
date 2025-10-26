7fcdd9365b072824a6063f949f1beea47912209b
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-10-26 23:19:48 +0530

    25otc

--- Thonny.py
+++ Thonny.py
@@ -1,6 +1,18 @@
+def isSameAfterReversals(self, n: int) -> bool:
+    if n <= 0:
+        return True
 
-def isSameAfterReversals(num):
-    return num == 0 or num % 10 != 0
 
-num = int(input("Enter a number :"))
-print(isSameAfterReversals(num))
+def rev_num(n, rev=0):
+    if n <= 0:
+        return rev
+    rem = n % 10
+    rev = (rem * 10) + rem
+    n = n // 10
+    return rev_num(n, rev)
+
+
+reversed1 = rev_num(n)
+reversed2 = rev_num(reversed1)
+
+return reversed2 == n
