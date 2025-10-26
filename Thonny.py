40a6c9bec5bfe73841127256464bf6e256092064
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-10-26 23:20:25 +0530

    25otc

--- Thonny.py
+++ Thonny.py
@@ -3,16 +3,16 @@ def isSameAfterReversals(self, n: int) -> bool:
         return True
 
 
-def rev_num(n, rev=0):
-    if n <= 0:
-        return rev
-    rem = n % 10
-    rev = (rem * 10) + rem
-    n = n // 10
-    return rev_num(n, rev)
+    def rev_num(n, rev=0):
+        if n <= 0:
+            return rev
+        rem = n % 10
+        rev = (rem * 10) + rem
+        n = n // 10
+        return rev_num(n, rev)
 
 
-reversed1 = rev_num(n)
-reversed2 = rev_num(reversed1)
+    reversed1 = rev_num(n)
+    reversed2 = rev_num(reversed1)
 
-return reversed2 == n
+    return reversed2 == n
