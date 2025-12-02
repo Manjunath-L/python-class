eab0b2434b7eb210e053dfe9ed7dfb6226938fb8
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-12-02 22:42:52 +0530

    2dec

--- data_str_in_python/SET/has_frozen_set.py
+++ data_str_in_python/SET/has_frozen_set.py
@@ -4,20 +4,20 @@ print(len(a))
 print(hash(10))
 
 
-b = {"Rama","krishna","Rama"}
+b = {"Rama", "krishna", "Rama"}
 print(b)
 print(len(b))
 print(hash("Rama"))
 
-c = {10.1,20.2,30.3}
+c = {10.1, 20.2, 30.3}
 print(c)
 print(hash(20.2))
 
-d = {True,False,False,True}
+d = {True, False, False, True}
 print(d)
 print(hash(True))
 
-# se1 = frozenset([10,20,30,40])
-# print(se1)
-# se1.add(10)
-# se1.remove(10)
+se1 = frozenset([10, 20, 30, 40])
+print(se1)
+se1.add(10)
+se1.remove(10)
