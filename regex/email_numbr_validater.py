2d351029ad94b2bf5cff5313927c48b58d5da854
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2026-02-12 16:19:00 +0530

    python-class

--- regex/email_numbr_validater.py
+++ regex/email_numbr_validater.py
@@ -1,1 +1,13 @@
-email_id = r"^[a-zA-Z0-9.]"
+import re
+email_id = r"^[a-zA-Z0-9._+%-]+@(gmail|yahoo|pentagonspace)(\.com|\.in)$"
+mob_num = r"^(\+91[-\s]?|91[-\s]?)[6-9]\d{4}[-\s]?\d{5}$"
+email = input("Enter your email: ")
+mobile = input("Enter your mobile number: ")
+if re.match(mob_num,mobile):
+    print("Your mobile number is valid")
+else:
+    print("Your mobile number is not valid")
+if re.match(email_id,email):
+    print("Your email is valid")
+else:
+    print("Your email is not valid")
